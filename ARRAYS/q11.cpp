a815dceccb3c53f7f915fd11678c5c3b941ca7e3
sunny jha <78363637+Sunnyjha007@users.noreply.github.com>
2023-03-30 20:36:11 +0530

    Create q11.cpp

---
+++ ARRAYS/q11.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+//   O(numRows^2)
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> v;
+        for(int i=0;i<numRows;i++){
+            vector<int> temp(i+1,1);
+            for(int j=1;j<i;j++){
+                temp[j] = v[i-1][j-1]+v[i-1][j];
+            }
+            v.push_back(temp);
+        }
+        return v;
+    }
+};
