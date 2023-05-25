c877cfcef62a12324f7b34c5423f8b1da4014232
sunny jha <78363637+Sunnyjha007@users.noreply.github.com>
2023-05-25 13:28:39 +0530

    Update q25.cpp

--- ARRAYS/Medium/q25.cpp
+++ ARRAYS/Medium/q25.cpp
@@ -8,10 +8,33 @@ bool solve(vector<int>& nums,vector<int>& dp,int idx){
     }
     return dp[idx] = 0;
 }
+bool solve(vector<int>& nums,vector<int>& dp){
+    dp[nums.size()-1] = 1;
+    for(int i=nums.size()-2;i>=0;i--){
+        for(int j=1;j<=nums[i];j++){
+            if(i+j<nums.size()&&dp[i+j]==1){
+                dp[i] = 1;break;
+            }
+        }
+    }
+    return dp[0];
+}
+bool solve(vector<int>& nums){
+    int n = nums.size();
+    int max_reach = 0;
+    for(int i=0;i<n;i++){
+        if(max_reach<i)return false;
+        max_reach = max(max_reach,i+nums[i]);
+    }
+    return true;
+}
 public:
     bool canJump(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n,-1);
-        return solve(nums,dp,0);
+        // vector<int> dp(n,-1);
+        // return solve(nums,dp,0);
+        // vector<int> dp(n,0);
+        // return solve(nums,dp);
+        return solve(nums);
     }
 };
