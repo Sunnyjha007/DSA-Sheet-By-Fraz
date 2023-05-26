8c95595269664f7d7349e48ad019882bdc7f675c
sunny jha <78363637+Sunnyjha007@users.noreply.github.com>
2023-05-26 12:31:40 +0530

    Update q1.cpp

--- ARRAYS/Hard/q1.cpp
+++ ARRAYS/Hard/q1.cpp
@@ -1,6 +1,26 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
+       int n = nums.size();
+       for(int i=0;i<n;i++){
+           int ele = nums[i];
+           if(ele>=1&&ele<=n){
+                int idx = ele - 1;
+               if(nums[idx]!=ele){
+                    swap(nums[idx],nums[i]);
+                    i--;
+               }   
+           }
+       }
+       for(int i=0;i<n;i++)if(i+1!=nums[i])return i+1;
+       return n+1;
+    }
+};
+
+
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
         int n = nums.size();
         sort(nums.begin(),nums.end());
         int ans=1;
