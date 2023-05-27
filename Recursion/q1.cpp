28f413cb5fd306f9cc126049dc72d7ae159e02ff
sunny jha <78363637+Sunnyjha007@users.noreply.github.com>
2023-05-27 17:15:35 +0530

    Create q1.cpp

---
+++ Recursion/q1.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+       if(n==0)return 1;
+       double ans = myPow(x,n/2);
+       if(n%2==0){
+           return ans*ans;
+       }else{
+           return (n>=0)?x*ans*ans:1/x*ans*ans;
+       }
+    }
+};
